7c59a3a9366c4fe399815a1d2f631d7b61f8905e
Matthew Kendon <matt@outlandish.com>
2025-02-11 14:48:08 +0000

    removed unused function

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,23 +1,4 @@
-import Link from 'next/link'
-
 import { ContainerInner, ContainerOuter } from '@/components/Container'
-
-function NavLink({
-  href,
-  children,
-}: {
-  href: string
-  children: React.ReactNode
-}) {
-  return (
-    <Link
-      href={href}
-      className="transition hover:text-teal-500 dark:hover:text-teal-400"
-    >
-      {children}
-    </Link>
-  )
-}
 
 export function Footer() {
   return (
